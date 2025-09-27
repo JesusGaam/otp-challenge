23ae58c30d76c42bf0512f0238eedb2fa3ab6368
Jesús García <jesusgarciaamaro@gmail.com>
2025-09-27 01:03:16 -0600

    Component styling has started

--- src/components/organimns/OtpForm.tsx
+++ src/components/organimns/OtpForm.tsx
@@ -1,20 +1,45 @@
 import { } from 'react'
+import { useOtpForm } from '../../hooks';
 import OtpInput from '../molecules/OtpInput'
-import { useObtainOtp } from '../../hooks';
+import Button from '../atoms/Button';
 
 const OtpForm = () => {
-  const { otp, error } = useObtainOtp();
-  console.log(otp, error);
+  const {
+    inputOtp,
+    receivedOtp,
+    receivedOtpError,
+    isValidOtp,
+    onInputOtpChange,
+  } = useOtpForm();
 
   return (
     <div className="p-4 max-w-md mx-auto">
-      <h2 className="text-lg font-semibold mb-4">OTP Verification</h2>
-      <p className="mb-4">Please enter the OTP confirmation:</p>
-      <OtpInput
-        otp2Validate={otp!}
-        disabled={error !== null}
-      />
-      {error && <div className="text-yellow-400">{error}</div>}
+      <h2 className="font-semibold text-white/90 md:text-[34px] sm:text-[22px] md:text-center sm:text-left">Te enviamos un SMS</h2>
+      <p className="font-normal text-sub-head sm:text-left md:text-center my-3 text-white/70">Ingresa el código que te enviamos al +50 (88) 888 888:</p>
+      <div className='flex flex-col items-center mb-3 gap-3'>
+        <OtpInput
+          disabled={receivedOtpError !== null}
+          onChange={onInputOtpChange}
+        />
+        <Button
+          variant={isValidOtp ? 'primary' : 'disabled'}
+        >
+          Continuar
+        </Button>
+      </div>
+
+      {receivedOtpError && <div className="text-yellow-400">{receivedOtpError}</div>}
+      {receivedOtp && !isValidOtp && <div className="text-red-500">The entered OTP is incorrect.</div>}
+      {isValidOtp && <div className="text-green-500">OTP is valid!</div>}
+      <div className="mt-4">
+        <strong>Received OTP:</strong> {receivedOtp || 'N/A'}
+      </div>
+      <div className="mt-2">
+        <strong>Entered OTP:</strong> {inputOtp || 'N/A'}
+      </div>
+      <div className="mt-2">
+        <strong>OTP Validity:</strong> {isValidOtp ? 'Valid' : 'Invalid'}
+      </div>
     </div>
   )
 }
