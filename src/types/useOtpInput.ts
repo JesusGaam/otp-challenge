0b7d17ba8fbd85d7a9084cd8d68f7626b57f0a4a
Jesús García <jesusgarciaamaro@gmail.com>
2025-09-26 23:25:54 -0600

    Types are created for useOtpInput hooks and forms

---
+++ src/types/useOtpInput.ts
@@ -0,0 +1,13 @@
+export type OtpValues = [string, string, string, string];
+
+export interface UseOtpInputReturn {
+  inputRefs: React.MutableRefObject<(HTMLInputElement | null)[]>;
+  otpList: OtpValues;
+  otpValue: string;
+  isValidOtp: boolean;
+  handleChange: handleOtpChangeType;
+  handleKeyDown: (index: number, e: React.KeyboardEvent) => void;
+  handlePaste: (e: React.ClipboardEvent<HTMLInputElement>) => void;
+}
+
+export type handleOtpChangeType = (index: number, value: string) => void;
