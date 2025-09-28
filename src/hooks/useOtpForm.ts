1a5fdbceb1dd8448bb3a843cb7c4aae1a92c17d8
Jesús García <jesusgarciaamaro@gmail.com>
2025-09-27 23:24:56 -0600

    handleResendOtp function was added to generate a random otp and refresh view

--- src/hooks/useOtpForm.ts
+++ src/hooks/useOtpForm.ts
@@ -1,15 +1,25 @@
-
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import { useObtainOtp } from '../hooks';
+import { generateRandomNumber } from '../utils';
 
 const useOtpForm = () => {
+  const navigate = useNavigate();
   const [inputOtp, setInputOtp] = useState<string>('');
   const [isValidOtp, setIsValidOtp] = useState<boolean>(false);
-  const { otp: receivedOtp, error: receivedOtpError } = useObtainOtp();
+  const {
+    otp: receivedOtp,
+    isError: isErrorReceivedOtp,
+    errorMessage: receivedOtpError
+  } = useObtainOtp();
 
   const onInputOtpChange = (otp: string) => {
     setInputOtp(otp);
   };
+  const handleResendOtp = () => {
+    const newCode = generateRandomNumber(4);
+    navigate(`/otp-verification?otp=${newCode}`, { replace: true });
+  }
 
   useEffect(() => {
     setIsValidOtp(inputOtp.length > 0 && inputOtp === receivedOtp);
@@ -18,9 +28,11 @@ const useOtpForm = () => {
   return {
     inputOtp,
     receivedOtp,
+    isErrorReceivedOtp,
     receivedOtpError,
     isValidOtp,
     onInputOtpChange,
+    handleResendOtp
   };
 };
 
