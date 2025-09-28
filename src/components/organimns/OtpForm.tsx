6b0fed4b26ef3a507e5a9c42c85fa072c49a48aa
Jesús García <jesusgarciaamaro@gmail.com>
2025-09-27 21:20:19 -0600

    form responsive styles where finished.

--- src/components/organimns/OtpForm.tsx
+++ src/components/organimns/OtpForm.tsx
@@ -3,44 +3,43 @@ import { useOtpForm } from '../../hooks';
 import OtpInput from '../molecules/OtpInput'
 import Button from '../atoms/Button';
 
-const OtpForm = () => {
+interface OtpFormProps {
+  className?: string;
+}
+
+const OtpForm: React.FC<OtpFormProps> = ({ className = '' }) => {
   const {
-    inputOtp,
     receivedOtp,
     receivedOtpError,
     isValidOtp,
     onInputOtpChange,
   } = useOtpForm();
-
   return (
-    <div className="p-4 max-w-md mx-auto">
-      <h2 className="font-semibold text-white/90 md:text-[34px] sm:text-[22px] md:text-center sm:text-left">Te enviamos un SMS</h2>
-      <p className="font-normal text-sub-head sm:text-left md:text-center my-3 text-white/70">Ingresa el código que te enviamos al +50 (88) 888 888:</p>
-      <div className='flex flex-col items-center mb-3 gap-3'>
-        <OtpInput
-          disabled={receivedOtpError !== null}
-          onChange={onInputOtpChange}
-        />
+    <div className={`flex flex-col items-center justify-between gap-0 md:gap-12 w-full ${className}`}>
+      <div className='flex flex-col gap-2 w-full'>
+        <h1 className="font-semibold text-white/90 text-[22px] md:text-[34px] text-left md:text-center">Te enviamos un SMS</h1>
+        <p className="font-normal text-[16px] text-white/70 text-left md:text-center">
+          Ingresa el código que te enviamos al +50 (88) 888 888:
+        </p>
+        <div className="flex flex-col items-center pt-8">
+          <OtpInput
+            disabled={receivedOtpError !== null}
+            onChange={onInputOtpChange}
+          />
+        </div>
+
+        {receivedOtpError && <div className="text-yellow-400">{receivedOtpError}</div>}
+        {receivedOtp && !isValidOtp && <div className="text-red-500">The entered OTP is incorrect.</div>}
+        {isValidOtp && <div className="text-green-500">OTP is valid!</div>}
+      </div>
+      <div>
         <Button
           variant={isValidOtp ? 'primary' : 'disabled'}
         >
           Continuar
         </Button>
       </div>
-
-      {receivedOtpError && <div className="text-yellow-400">{receivedOtpError}</div>}
-      {receivedOtp && !isValidOtp && <div className="text-red-500">The entered OTP is incorrect.</div>}
-      {isValidOtp && <div className="text-green-500">OTP is valid!</div>}
-      <div className="mt-4">
-        <strong>Received OTP:</strong> {receivedOtp || 'N/A'}
-      </div>
-      <div className="mt-2">
-        <strong>Entered OTP:</strong> {inputOtp || 'N/A'}
-      </div>
-      <div className="mt-2">
-        <strong>OTP Validity:</strong> {isValidOtp ? 'Valid' : 'Invalid'}
-      </div>
-    </div>
+    </div >
   )
 }
 
