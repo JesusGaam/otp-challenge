e16f1ab58611d2d348d114595b72a8e824b9b823
Jesús García <jesusgarciaamaro@gmail.com>
2025-09-27 21:21:33 -0600

    css responsive page styles were finished

--- src/components/pages/OtpVerification.tsx
+++ src/components/pages/OtpVerification.tsx
@@ -2,10 +2,11 @@ import { } from 'react'
 import OtpForm from "../organimns/OtpForm";
 
 const OtpVerification = () => {
-
   return (
-    <div>
-      <OtpForm />
+    <div className='min-h-screen px-6 md:px-8 flex flex-col justify-center items-center'>
+      <div className='w-full md:max-w-[700px]'>
+        <OtpForm className='min-h-screen md:min-h-auto py-8 md:p-0'/>
+      </div>
     </div>
   )
 }
